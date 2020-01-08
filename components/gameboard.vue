1104bb621e89dcb941474359b3c21a3c38cce84e
沢田悠佳 <YukaSawata@yuka-no-MacBook-Air.local>
2020-01-08 19:39:30 +0900

    toadsandfrogs

--- components/gameboard.vue
+++ components/gameboard.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
   <div v-if="teban!=null">
-    {{teban==2? "F":"T"}}
-    </div>
+    You are  
+    {{teban==2? "F":"T"}},
+    and it is
+    {{games["game"].teban==2? "F":"T"}}'s turn.
+  </div>
   <table border v-if="started">
     <tr>
       <td v-for="(k,i) in JSON.parse(games[0].board)" @click="move(i)" :class="species(k)" style="text-align:center">
@@ -13,7 +16,14 @@
   <b-button @click="start" v-if="!started">
     start
   </b-button>
-  {{games[0]}}
+  <div v-if="started">
+    <div v-if="games[0].finished">
+      Finish
+    </div>
+  </div>
+  <div>
+    {{thegame}}
+  </div>
 </div>
 </template>
 
@@ -36,7 +46,10 @@ export default{
 	this.$store.dispatch('setGamesRef', db.collection('games'))//setGamesRefを呼び出し、gemesを
     },
     computed: {
-	    ...mapGetters({games: 'games' })
+	thegame(){
+	    return db.collection("games").doc("game")
+	},
+	    ...mapGetters({games: 'games' }),
     },
     methods:{
 	move(i){//i番目のセルが押された時
@@ -44,7 +57,8 @@ export default{
 	    let ref = db.collection("games").doc("game")//firebabaseのcollectionの中のgamesの中のgameを呼び出す
 	    ref.get().then(function(doc){//docを呼び出し　thenという関数の中のfunctionという関数を呼び出してる
 		let board=JSON.parse(doc.data().board)//localにボードデータを保存
-		console.log(board)
+		let teban = doc.data().teban//firbaseの上にあるデータをlocalのteban変数に確認
+		//console.log(board)
 		//ゲームの中読み出して
 		if(board[i]!=doc.data().teban || kore.teban!=board[i]){//手番が違うコマを動かそうとしたら何もしない
 		    return;
@@ -80,9 +94,83 @@ export default{
 		else if(doc.data().teban==2){
 		    ref.update({teban:1})
 		}
-		})
+		let nextteban = 3-doc.data().teban
+		let found = false
+		for(let i in board){
+		    console.log("i:" + i)
+		    console.log("teban:"+nextteban)
+		    console.log(board)
+		    console.log(board[i])
+		    if (i+1 < board.length && board[i*1+1]==0 ){
+			console.log("it's true")
+		    }
+		    if(board[i]!=nextteban){
+			continue
+		    }
+		    if(i+1<board.length && board[i*1+1]==0 && board[i]==1){
+			console.log("a")
+			found=true
+			break
+		    }
+		    if(i+2<board.length && board[i*1+2]==0 && board[i*1+1]==2 && board[i]==1){
+			console.log("b")
+			found=true
+			break
+		    }
+		    if(i>0 && board[i-1]==0 && board[i]==2){
+			console.log("c")
+			found=true
+			break
+		    }
+		    if(i>1 && board[i-2]==0 && board[i-1]==1 && board[i]==2){
+			console.log("d")
+			found=true
+			break
+		    }
+		}
+		if (!found){
+		    ref.update({numplayer:0})//refとはデータベースへのリファレンス
+		    ref.update({finished:true})
+		}
+	    })	    
+	},
 	
-	   
+	finished(){
+	    //console.log(board)
+	    //console.log(teban)
+	    let kore=this
+	    let ref = db.collection("games").doc("game")//firebabaseのcollectionの中のgamesの中のgameを呼び出す
+	    ref.get().then(function(doc){//docを呼び出し　thenという関数の中のfunctionという関数を呼び出してる
+		let board=JSON.parse(doc.data().board)//localにボードデータを保存
+		let teban = doc.data().teban//firbaseの上にあるデータをlocalのteban変数に確認
+		console.log(board)
+		for(let i in board){
+		    if(board[i]!=teban){
+			continue
+		    }
+		    if(i+1<board.length && board[i+1]==0 && teban==1){
+			return
+		    }
+		    if(i+2<board.length && board[i+2]==0 && board[i+1]==2 && teban==1){
+			return
+		    }
+		    if(i>0 && board[i-1]==0 && board[i]==2){
+			console.log("a")
+			console.log(board)
+			console.log(teban)
+			return
+		    }
+		    if(i>1 && board[i-2]==0 && board[i-1]==1 && board[i]==2){
+			console.log("b")
+			console.log(board)
+			console.log(teban)
+			return
+		    }
+		}
+		ref.update({numplayer:0})//refとはデータベースへのリファレンス
+		//fnd=true
+		ref.update({finished:true})
+	    })
 	},
 	species(k){//kは1,2,"_"
 	    if(k==1){
@@ -105,12 +193,24 @@ export default{
 	start(){
 	    let kore=this
 	    let ref = db.collection("games").doc("game")
+	    
+	    let board=[]
+	    for(let i=0; i<this.M;i++){
+		board.push(1)
+	    }
+	    for(let i=0;i<this.L-(this.M*1+this.N*1);i++){
+		board.push(0)
+	    }
+	    for(let i=0;i<this.N;i++){
+		board.push(2)		
+	    }
+	    
 	    ref.get().then(function(doc){
 		if(doc.data().numplayer>=2){
 		    return;
 		}
 		kore.teban=doc.data().numplayer+1
-		ref.update({board:"[1,1,0,0,2,2]",teban:1,numplayer:kore.teban})
+		ref.update({board:JSON.stringify(board),teban:1,numplayer:kore.teban,finished:false})
 	    })	    
 	    this.started=true		
 	}
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,86 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">   
-      <gameboard M=2 N=3 L=7>
-		
-	  </gameboard>
-    
+<section class="section">
+  <div class="tile is-ancestor">
+      <div class="tile is-vertical is-8">
+	<div class="tile">
+	  <div class="tile is-parent is-vertical">
+	    <article v-if="!started" class="tile is-child notification is-warning">
+	      <ul>
+		<li v-for="(g,i) in games">
+		  <b-button @click="startgame(g.id)"> 
+		  {{i}}:{{g.board}}:{{g.id}}
+		   </b-button>
+		</li>		
+	      </ul>
+            </article>
+            <article v-if="!started"class="tile is-child notification is-primary">
+	      <b-field label="number of toads">
+		<b-numberinput v-model="M"></b-numberinput>
+	      </b-field>
+	      <b-field label="number of frogs">
+		<b-numberinput v-model="N"></b-numberinput>
+	      </b-field>
+	      <b-field label="board length">
+		<b-numberinput v-model="L"></b-numberinput>
+	      </b-field>
+	      <b-button v-if="!started" @click="newgame"> 
+		new game
+	      </b-button>
+	    </article>
+	  </div>
+	</div>
+	</div>
+	<div v-if="started">
+	  <gameboard M=2 N=3 L=7>
+      </gameboard>
     </div>
-  </section>
+    </div>
+    
+</section>
 </template>
 
 <script>
 import gameboard from '~/components/gameboard'
-export default {
-  name: 'HomePage',
+import firebase from '~/plugins/firebase.js'
+const db = firebase.firestore()
+import { mapGetters } from 'vuex'
 
-  components: {
-      gameboard
-      
-  }
+export default {
+    name: 'HomePage',
+    data(){
+	return{started:false,M:2,N:2,L:6
+	      }
+    },
+    created(){//dbcollectionとgemeを結びつける
+	this.$store.dispatch('setGamesRef', db.collection('games'))//setGamesRefを呼び出し、gemesを
+    },
+    computed: {
+	    ...mapGetters({games: 'games' }),
+    },
+    components: {
+	gameboard
+	
+    },
+    methods:{
+	newgame(){
+	    let ref=db.collection("games")
+	    let board=[]
+	    for(let i=0; i<this.M;i++){
+		board.push(1)
+	    }
+	    for(let i=0;i<this.L-(this.M*1+this.N*1);i++){
+		board.push(0)
+	    }
+	    for(let i=0;i<this.N;i++){
+		board.push(2)		
+		}
+	    ref.doc().set({board:JSON.stringify(board),teban:1,numplayer:0,finished:false})
+	},
+	startgame(id){
+	    this.started=true;
+	    }
+    }
+    
 }
+
 </script>
